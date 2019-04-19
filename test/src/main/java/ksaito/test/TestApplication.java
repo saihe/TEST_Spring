e46990f0d08e6035494710def2f79e3e965e1794
p_satio.kyohe <p_saito.kyohei@headwaters.co.jp>
2019-04-19 18:05:07 +0900

    2019/04/19 ksaito プロジェクト「test」にてAPI作成 外部ファイルのymlを読み込むよう設定 Controller「TestController」を作成しURL「localhost:8080/test/api」でアクセスできるように実装 application.ymlとlog.ymlを作成し値を読むように実装

--- test/src/main/java/ksaito/test/TestApplication.java
+++ test/src/main/java/ksaito/test/TestApplication.java
@@ -1,13 +1,18 @@
 package ksaito.test;
 
+import ksaito.test.Utilities.PropertiesUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TestApplication {
+    @Autowired
+    private PropertiesUtil prop;
 
-	public static void main(String[] args) {
-		SpringApplication.run(TestApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TestApplication.class, args);
+    }
 
 }
