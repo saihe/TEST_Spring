11aeaaee2a9342eca37162801eee59ba38b5d389
p_satio.kyohe <p_saito.kyohei@headwaters.co.jp>
2019-04-25 14:50:25 +0900

    2019/04/25 ksaito 外部ライブラリプロジェクト作成

---
+++ TestModule/src/main/java/ksaito/testModule/Module01.java
@@ -0,0 +1,14 @@
+package ksaito.testModule;
+
+/**
+ * モジュール01 コンソール出力ユーティリティクラス
+ */
+public class Module01 {
+    /**
+     * コンソール出力メソッド
+     * @param message
+     */
+    public static void print(String message) {
+        System.out.println(message);
+    }
+}
