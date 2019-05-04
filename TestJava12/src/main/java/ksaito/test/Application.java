277f50ff6d4fe5277c3b0cfea3c09d4c9d4eef7b
saihe.kyon@gmail.com <saihe.kyon@gmail.com>
2019-05-04 21:43:38 +0900

    java12 練習用プロジェクト

---
+++ TestJava12/src/main/java/ksaito/test/Application.java
@@ -0,0 +1,15 @@
+package main.java.ksaito.test;
+
+public class Application {
+
+	public static void main(String[] args) {
+		print("Hello world.");
+
+	}
+
+	private static void print(String message) {
+		System.out.println(message);
+
+	}
+
+}
